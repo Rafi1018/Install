c0c0a02c04ae45fc14355c90cf36b6b0b6e2c431
Rafi1018 <142591064+Rafi1018@users.noreply.github.com>
2024-02-15 19:10:56 +0600

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,4 +1,6 @@
 pacman -S --needed base-devel xorg-server xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon sudo pulsaudio pavucontrol nano git pacman-contrib xdg-user-dirs
+mv dwm.desktop /usr/share/xsessions/
+chmod 644 /usr/share/xsessions/dwm.desktop
 systemctl enable paccache.timer
 useradd --create-home rafi
 passwd rafi
